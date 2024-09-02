2239855ba301e5d0045e669a5938bfe795334bb8
ZERU Simon <zerus@hostname.iut2.upmf-grenoble.fr>
2024-09-02 14:08:45 +0200

    Produit fonctionnel

--- produit.c
+++ produit.c
@@ -4,6 +4,13 @@
 int main(void)
 {
 	/* à compléter: corps de main */
-
+	int a, b, c;
+	printf("programme qui multiplie 2 nombres");
+	printf("\nsaisir un premier nombre : ");
+	scanf("%d", &a);
+	printf("\nsaisir un deuxième nombre : ");
+	scanf("%d", &b);
+	c = a*b;
+	printf("%d x %d = %d", a, b, c);
 	return EXIT_SUCCESS;
 }
