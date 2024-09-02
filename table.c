d93cd480cddcac5fc60c495cf0b2610ddea96a9d
ZERU Simon <zerus@hostname.iut2.upmf-grenoble.fr>
2024-09-02 14:15:14 +0200

    table de multiplication fonctionnelle

---
+++ table.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+    int table, result;
+    printf("vous voulez la table de combien ? ");
+    scanf("%d", &table);
+    for (int i = 0; i < 11; i++) {
+        result = i * table;
+        printf("\n%d x %d = %d", i, table, result);
+    }
+}
