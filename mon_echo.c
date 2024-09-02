35a0c4ec8f4b0d1783d2baded562a1b7566889c2
ZERU Simon <zerus@hostname.iut2.upmf-grenoble.fr>
2024-09-02 15:01:06 +0200

    mon_echo fonctionnel

--- mon_echo.c
+++ mon_echo.c
@@ -1,9 +1,14 @@
 /* à compléter: entêtes */
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
 	/* à compléter: corps de main */
-
+	for (int i = 0; i < argc; i++) {
+		
+		printf('%s ', argv[i]);
+	}
 	return EXIT_SUCCESS;
 }
