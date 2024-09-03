c1e15764020d0ff1bdcec2db4a18a063a5d0a868
simon-zeru <sim.zeru@gmail.com>
2024-09-03 12:04:39 +0200

    bin2dec fonctionnelle

--- bin2dec.c
+++ bin2dec.c
@@ -2,3 +2,18 @@
 #include <string.h>
 #include <stdio.h>
 
+void bin2dec(char bin[], char dec[]) {
+    size_t size = strlen(bin);
+    if (size == 0) {
+        return NULL;
+    }
+    int j = 0;
+    int result = 0;
+    for (int i = size - 1; i >= 0; i--) {
+        result += atoi(bin[i]) * 2 * j;
+        j = j + 2; 
+    }
+
+    
+    
+}
